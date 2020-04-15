9e9c8226b462da21ff911cf413d55b574aaa487d
Inho Song <mearrong123@gmail.com>
2020-04-16 03:06:59 +0900

    16 Apr 2020 AM 03:00+ FCFS Complete. (critical functions revised)

--- lab1_sched_test.c
+++ lab1_sched_test.c
@@ -93,7 +93,7 @@ E       2       8           E   4   4
     int test=0;
     policy= FCFS_SCHED;
     test1(policy);
-
+    test2(policy);
     // policy = ROUND_ROBIN_SCHED;
     // test1(policy);
 
