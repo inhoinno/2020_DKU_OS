3d2227db2d0311da317ae8980f51dd4bc1f6d3b9
Inho Song <mearrong123@gmail.com>
2020-05-02 01:33:19 +0900

    01May third build : tree NULL case + hand-over-hand

--- lab2_sync/lab2_bst.c
+++ lab2_sync/lab2_bst.c
@@ -264,31 +264,34 @@ int lab2_node_remove(lab2_tree *tree, int key)
     //I . Traverse
     //II. Execute from "remove"
     */
+
+    lab2_node *premove = NULL;
     lab2_node *remove = tree->root;
-    lab2_node *pre = tree->root;
-    lab2_node *successor;
+    lab2_node *pleaf = tree->root;
+    lab2_node *leaf = tree->root;
     lab2_node *psuccessor;
+    lab2_node *successor;
     lab2_node *child;
     int state = 0; //FALSE
 
-    while (remove != NULL)
+    while (leaf != NULL)
     {
-        pre = remove;
+        premove = remove;
+        remove = leaf;
         if (remove->key == key)
-            break;
+            break; //i find a key 
         else if (remove->key < key)
-            remove = remove->right;
+            leaf = leaf->right;
         else
-            remove = remove->left;
+            leaf = leaf->left;
     }
     /*#2 execution
         1. simple deletion
         2. complex deletion
     */
-    if (remove != NULL)
+    if (remove != NULL) //i find value
     {
         //state = true
-
         //else if remove has a two child
         if (remove->left != NULL && remove->right != NULL)
         {
@@ -312,16 +315,38 @@ int lab2_node_remove(lab2_tree *tree, int key)
         else if (remove->left == NULL || remove->right == NULL)
         {
             child = (remove->right == NULL) ? remove->left : remove->right;
-            if (pre->left->key == remove->key)
-                pre->left = child;
-            else if (pre->right->key == remove->key)
-                pre->right = child;
-            free(remove);
-            remove = NULL;
+            if(child ==NULL) //Case No Child
+            {
+                if(remove == tree->root){
+                    //tree Deletion
+                    tree->root = NULL;
+                    free(remove); remove = NULL;
+                } 
+                else{// case General
+                    if(premove->left == remove)
+                        premove->left =NULL;
+                    else premove->right = NULL;
+                    free(remove); remove = NULL;
+                }
+            }
+            else // Case one Child
+            {
+                if(remove == tree->root){
+                    //Tree->root = child;
+                    tree->root = child;
+                    free(remove); remove = NULL;
+                } 
+                else{// case General
+                    if(premove->left == remove)
+                        premove->left = child;
+                    else premove->right = child; 
+                    free(remove); remove = NULL;
+                }
+            }
             state = 1; //TRUE
         }
     }
-    else
+    else //Cant find value
         state = 0;                              //"No such Key"
     return (state) ? LAB2_SUCCESS : LAB2_ERROR; //error : No such Key
 }
